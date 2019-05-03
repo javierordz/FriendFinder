e88c748c083d68d55ef36d73268b7c7d47faf3b3
javierordz <javier.ordz@gmail.com>
2019-05-03 17:34:22 -0500

    home API footer added

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@
 var friends = [
     {
         name: "Abraham Lincoln",
-        photo: "https://sharetngov.tnsosfiles.com/tsla/exhibits/lincoln/images/2429.jpg",
+        pic: "https://sharetngov.tnsosfiles.com/tsla/exhibits/lincoln/images/2429.jpg",
         scores: [ 5, 1, 4, 3, 5, 1, 2, 5, 4, 2 ]
     },
     {
         name: "Ash Ketchum",
-        photo: "https://media.pocketmonsters.net/characters/2.png",
+        pic: "https://media.pocketmonsters.net/characters/2.png",
         scores: [ 3, 2, 5, 1, 1, 4, 5, 3, 2, 2 ]
     },
     {
         name: "Keanu Reeves",
-        photo: "https://m.media-amazon.com/images/M/MV5BNjUxNDcwMTg4Ml5BMl5BanBnXkFtZTcwMjU4NDYyOA@@._V1_UY317_CR15,0,214,317_AL_.jpg",
+        pic: "https://m.media-amazon.com/images/M/MV5BNjUxNDcwMTg4Ml5BMl5BanBnXkFtZTcwMjU4NDYyOA@@._V1_UY317_CR15,0,214,317_AL_.jpg",
         scores: [ 2, 2, 3, 4, 3, 2, 3, 4, 3, 2 ]
     },
     {
         name: "Bjork",
-        photo: "https://www.billboard.com/files/styles/article_main_image/public/media/Bjork-portrait-2016-billboard-1548.jpg",
+        pic: "https://www.billboard.com/files/styles/article_main_image/public/media/Bjork-portrait-2016-billboard-1548.jpg",
         scores: [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
     },
     {
         name: "Ada Lovelace",
-        photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2013/10/ay95895977ada-lovelace-engl1.jpg?quality=90&strip=all&zoom=1&resize=644%2C701&ssl=1",
+        pic: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2013/10/ay95895977ada-lovelace-engl1.jpg?quality=90&strip=all&zoom=1&resize=644%2C701&ssl=1",
         scores: [ 4, 1, 3, 4, 4, 3, 2, 4, 5, 3 ]
     },
   ];
