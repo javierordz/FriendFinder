444ca573006ddc75062f193d1e95c80231e3fb73
javierordz <javier.ordz@gmail.com>
2019-05-03 16:48:19 -0500

    modal html corrected, routing testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,55 @@
 // Routes
-
 app.get("/api/friends", function(req, res) {
   return res.json(friends);
 });
 
-app.post("/api/friends", function(req, res) {
-    // var newcharacter = req.body;
-  
-    // console.log(newcharacter);
-    // characters.push(newcharacter);
-  
-    // res.json(newcharacter);
+// Submit click
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  function validateForm() {
+    var isValid = true;
+    $(".form-control").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+
+    $(".selection").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+
+    return isValid;
+  }
+
+  // Valid form creates data array, posts api/friends
+  if (validateForm()) {
+    var userData = {
+      name: $("#name").val(),
+      pic: $("#pic").val(),
+      scores: [
+        $("#q1").val(),
+        $("#q2").val(),
+        $("#q3").val(),
+        $("#q4").val(),
+        $("#q5").val(),
+        $("#q6").val(),
+        $("#q7").val(),
+        $("#q8").val(),
+        $("#q9").val(),
+        $("#q10").val()
+      ]
+    };
+
+    $.post("/api/friends", userData, function(data) {
+      $("#friendName").text(data.name);
+      $("#friendPic").attr("src", data.pic);
+      $("#friend-results").modal("toggle");
+    });
+
+  } else {
+    alert("Please fill all fields to submit.");
+  }
 });
