684a523238559c4d2de5d826682121f12216c16b
javierordz <javier.ordz@gmail.com>
2019-05-03 18:03:16 -0500

    implemented survey-logic

--- app/public/survey-logic.js
+++ app/public/survey-logic.js
@@ -1,48 +1,49 @@
-// Submit click
+// SUBMIT CLICK
 $("#submit").on("click", function(event) {
     event.preventDefault();
   
     function validateForm() {
-      var isValid = true;
-      $(".form-control").each(function() {
-        if ($(this).val() === "") {
-          isValid = false;
-        }
-      });
+        var isValid = true;
+        $(".form-control").each(function() {
+            if ($(this).val() === "") {
+                isValid = false;
+            }
+        });
+        $(".selection").each(function() {
+            if ($(this).val() === "") {
+                isValid = false;
+            }
+        });
   
-      $(".selection").each(function() {
-        if ($(this).val() === "") {
-          isValid = false;
-        }
-      });
-  
-      return isValid;
+        return isValid;
     }
   
-    // Valid form creates data array, posts api/friends
+    // Create user & oost to /api/friends
     if (validateForm()) {
-      var userData = {
-        name: $("#name").val(),
-        pic: $("#pic").val(),
-        scores: [
-          $("#q1").val(),
-          $("#q2").val(),
-          $("#q3").val(),
-          $("#q4").val(),
-          $("#q5").val(),
-          $("#q6").val(),
-          $("#q7").val(),
-          $("#q8").val(),
-          $("#q9").val(),
-          $("#q10").val()
-        ]
-      };
+        var userData = {
+            name: $("#name").val(),
+            pic: $("#pic").val(),
+            scores: [
+                $("#q1").val(),
+                $("#q2").val(),
+                $("#q3").val(),
+                $("#q4").val(),
+                $("#q5").val(),
+                $("#q6").val(),
+                $("#q7").val(),
+                $("#q8").val(),
+                $("#q9").val(),
+                $("#q10").val()
+            ]
+        };
+
+        console.log(userData);
   
-      $.post("/api/friends", userData, function(data) {
-        $("#friendName").text(data.name);
-        $("#friendPic").attr("src", data.pic);
-        $("#friend-results").modal("toggle");
-      });
+        $.post("/api/friends", userData, function(data) {
+            $("#friendName").text(data.name);
+            $("#friendPic").attr("src", data.pic);
+            $("#friend-results").modal("toggle");
+        });
   
     } else {
       alert("Please fill all fields to submit.");
